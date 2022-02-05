1ae8c11dbb724cad65cac43559524d83e658672f
Nhi Truong <tamnhi96@gmail.com>
2022-02-04 23:53:30 -0800

    Default behavior of HTML forms

--- src/index.js
+++ src/index.js
@@ -19,12 +19,16 @@ app.set('views', path.join(__dirname, 'resources/views'));
 
 app.get('/', (req, res) => {
   res.render('home');
-})
+});
 
 app.get('/news', (req, res) => {
   res.render('news');
-})
+});
+
+app.get('/search', (req, res) => {
+  res.render('search');
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
-})
+});
