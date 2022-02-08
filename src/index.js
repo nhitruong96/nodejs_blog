56da9038b1ceb64b9bf1bbb915fdf569715a90ff
Nhi Truong <tamnhi96@gmail.com>
2022-02-07 18:47:42 -0800

    Install husky

--- src/index.js
+++ src/index.js
@@ -30,8 +30,8 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
 // Route init
-route(      app);
+route(app);
 
-            app.listen(port, () => {
-            console.log(`Example app listening at http://localhost:${port}`);
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
 });
