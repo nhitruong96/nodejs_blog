a18c1faa38db0477e7e2a5ea8137cfd2b44514b9
Nhi Truong <tamnhi96@gmail.com>
2022-02-13 17:47:07 -0800

    Update course

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
+const methodOverride = require('method-override');
 const { engine } = require('express-handlebars');
 const app = express();
 const port = 3000;
@@ -23,11 +24,16 @@ app.use(
 );
 app.use(express.json()); //XMLHttpRequest, fetch, axios, ajax
 
+app.use(methodOverride('_method'));
+
 //Template engine
 app.engine(
     'hbs',
     engine({
         extname: '.hbs',
+        helpers: {
+            sum: (a, b) => a + b,
+        },
     }),
 );
 app.set('view engine', 'hbs');
